a62c314cc93e78aee8aa6a0f841b53fedae23b08
Malcolm <mbalchcr@oberlin.edu>
2013-07-02 19:58:11 -0400

    initial commit

---
+++ Bloom/Assets/Manager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class Manager : MonoBehaviour {
+	
+	//Connect to editor
+	public GameObject s_system;
+	
+	
+	// Use this for initialization
+	void Start () {
+		print("Starting home screen");
+		
+		//load home solar system
+		//load 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
