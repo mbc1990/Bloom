5cb6f4dd41671af78b78328cc23b04777924d1f6
Malcolm <mbalchcr@oberlin.edu>
2013-07-17 00:15:05 -0400

    eval sketches

--- Bloom/Assets/Scripts/Missions/mission_data.cs
+++ Bloom/Assets/Scripts/Missions/mission_data.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /*
  * 
@@ -16,7 +17,7 @@ public class mission_data : MonoBehaviour {
 	//current destination
 	public GameObject dest;
 	
-	//list of gameobjects w/ module script & "interface" like module script that has the module name
+	//list of gameobjects w/ module script & interface-like module script that has the module name
 	public ArrayList modules = new ArrayList();
 	
 	//true when in orbit at a destination
@@ -27,6 +28,10 @@ public class mission_data : MonoBehaviour {
 	
 	//Used for correcting oval shaped orbit around moving planets (position of destination planet LAST frame)
 	Vector3 lastdestpos;
+	
+	//symbol tables for interpreter
+	//table of type floats
+	Dictionary<string,float> fl_table = new Dictionary<string,float>();
 	
 	//constants
 	//distance from target distination at which point the probe will stop travelling and begin orbitting
@@ -104,6 +109,15 @@ public class mission_data : MonoBehaviour {
 				//evaluate if condition, move exe_pos to end of if block if the condition is false
 			} else if(cur == "float") {
 				//variable change, use float table
+				//use case is float name = expression
+				string v_name = toks[exe_pos+1] as string;
+				//syntax error
+				if (toks[exe_pos + 2] != "=") {
+					print ("syntax error");
+					//somehow terminate code execution
+				}
+				
+				
 			} else if(cur == "body") {
 				//needs more thought, but this will be used to keep track of planets (for example, the nav module will return a list of local planets)
 			} else if (cur == "mod") {
@@ -112,6 +126,15 @@ public class mission_data : MonoBehaviour {
 				//shouldn't happen?
 			}
 		}
+	}
+	
+	/*
+	 * Evaluates a mathematical expression (floats only)
+	 * Recursive left-to-right evaluation (?)
+	 * Must also be able to evaluate module things to float values
+	 */
+	float eval_math_exp(ArrayList exp) {
+		return 0;
 	}
 	
 	/*
@@ -127,7 +150,8 @@ public class mission_data : MonoBehaviour {
 			}
 			//other var table cases here
 		} else {
-			//case of an equality check - compare the value before the == to the value after the == 	
+			//case of an equality check - compare the value before the == to the value after the ==
+			//probably use eval_math_exp here
 		}
 		return true;
 	}
